eafabd224ea036efb46e790e9fe4418b0b98a35e
Aneesh Pappu <apappu97@gmail.com>
2016-09-02 01:54:03 -0700

    added routes for getting scores for a current user and adding a new score for a current user where key is current date

--- WMe/routes/scores.js
+++ WMe/routes/scores.js
@@ -3,5 +3,41 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var User = require('../model/person');
 
+/* Get score for given user */
+router.get('/:phoneNumber', function(req, res, next){
+  User.find({phoneNumber: req.params.phoneNumber}, function(err, user){
+    if(err){
+      console.log("user not found");
+      res.send(400);
+    } else{
+      console.log("user found, sending score");
+      res.json(user.scores);
+    }
+  });
+});
+
+router.post('/', function(req, res, next){
+  newScore = req.body.score;
+  date = req.body.date;
+  User.find({phoneNumber: req.body.phoneNumber}, function(err, user){
+    if(err){
+      console.log("user not found");
+      res.send(400);
+    } else{
+      console.log("user found, setting score");
+      // Add newScore to 'scores' object map
+      user.scores[date] = newScore;
+      user.save(function(err){
+        if(err){
+          console.log("user not saved");
+          res.send(500);
+        } else{
+          console.log("user saved with updated score!");
+          res.send(200);
+        }
+      });
+    }
+  });
+});
 
 module.exports = router;
