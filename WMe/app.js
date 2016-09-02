bc7f673db86869a15985109a861bce13daf916b3
Aneesh Pappu <apappu97@gmail.com>
2016-09-02 01:43:59 -0700

    incorporated routes into app.js -- still need to be tested, after schema is agreed upon and calculating of current score is ironed out

--- WMe/app.js
+++ WMe/app.js
@@ -5,13 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+var mongoose = require('mongoose');
+var User = require('./model/person');
 var users = require('./routes/users');
+var friends = require('./routes/friends');
+var scores = require('./routes/scores');
 
 var app = express();
-
-var mongoose = require('mongoose')
-var User = require('./model/person')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,8 +25,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+
 app.use('/users', users);
+app.use('/friends', friends);
+app.use('./scores', scores);
+
+app.get('/', function(req, res, next){
+  console.log("index page hit");
+  res.send(200);
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
